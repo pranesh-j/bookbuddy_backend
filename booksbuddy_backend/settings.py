53ec8c4aecadc7a6bb189aef1c0163c142f72c42
pranesh-j <praneshjahagirdar8888@gmail.com>
2024-11-23 17:54:54 +0530

    Fix CORS and add proper logging

--- booksbuddy_backend/settings.py
+++ booksbuddy_backend/settings.py
@@ -54,7 +54,7 @@ CORS_ALLOWED_ORIGINS = [
 ALLOWED_HOSTS = [
     '*',
     '.vercel.app',
-    'bookbuddy-backend-2f6xdk2dy-praneshs-projects-6f1c158f.vercel.app',
+    'bookbuddy-backend.vercel.app',
     'localhost',
     '127.0.0.1'
 ]
@@ -64,6 +64,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://bookbuddy-frontend-ooi53kv5-praneshs-projects-6f1c158f.vercel.app",
     "https://bookbuddy-frontend-git-main-praneshs-projects-6f1c158f.vercel.app"
 ]
+
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
